66aa62128f686f2eaeb393c35d4c9fa96c192308
Zack <CSMZOSGO@ljmu.ac.uk>
2023-04-19 16:49:58 +0100

    all insight code

--- folder1.py
+++ folder1.py
@@ -6,6 +6,8 @@ with open("Video_Games_Sales_as_at_22_Dec_2016.csv","r") as file:
     header_row = next(csv_reader)
     dataframe = pd.read_csv("Video_Games_Sales_as_at_22_Dec_2016.csv")
     
+    maximiumVal = dataframe.max()
+    minimumVal = dataframe.min()
 
 #define a fuction for the three options and main menu     
 
@@ -55,17 +57,66 @@ with open("Video_Games_Sales_as_at_22_Dec_2016.csv","r") as file:
 
     def option2():
         print("== Please select the insight you would like to view==\n")
-        print("1.Best game by critic_score")
-        print("2.Best platform by critic_score")
+        print("1.Best games by critic_score")
+        print("2.Best platforms by critic_score")
         print("3.What year sold the most games")
-        print("4.The most average game")
-        print("5.Average global sales for platforms")
-        print("6.Average Sales per region per publisher")
-        print("7.Median sales per year")
-        print("8.Mean sales per region")
-        print("9.mininum global sales for a game")
-        userInsights = input("Please enter the insight number you would like to view:\n")
-       
+        print("4.The most average games")
+        print("5.Average global sales for each platform")
+        print("6.Average Sales per region")
+        print("7.Median of year of release")
+        print("8.mininum global sales for a game")
+        userInsight = int(input("Please enter the insight number you would like to view:\n"))
+
+        if userInsight == 1:
+            gameNames = dataframe[["Name","Critic_Score"]].copy()
+            maxScore = maximiumVal.loc["Critic_Score"]
+            print(gameNames.loc[gameNames["Critic_Score"] == maxScore])
+
+        elif userInsight == 2:
+            platformdf = dataframe[["Platform","Critic_Score"]].copy()
+            maxScore = maximiumVal.loc["Critic_Score"]
+            print(platformdf.loc[platformdf["Critic_Score"] == maxScore])
+            
+        elif userInsight == 3:
+            df = dataframe[["Year_of_Release","Global_Sales"]]
+            yearArr = [0,0] 
+            for i in range (int(minimumVal.loc["Year_of_Release"]),int(maximiumVal["Year_of_Release"])):
+                tempdf = df[df["Year_of_Release"] == i]
+                tempSum = tempdf["Global_Sales"].sum()
+                if tempSum > yearArr[1]:
+                    yearArr = [i,tempSum]
+            print("{} has the largest global sales with {} million units sold ".format(yearArr[0],int(yearArr[1])))        
+            
+        elif userInsight == 4:
+            gameNames = dataframe[["Name","Critic_Score"]].copy()
+            meanScore = gameNames["Critic_Score"].mean()
+            print(gameNames.loc[gameNames["Critic_Score"] == int(meanScore)])
+
+        elif userInsight == 5:
+            platformdf = dataframe[["Platform","Global_Sales"]].copy()
+            platformSeries = dataframe["Platform"].unique()
+            for i in range(0, len(platformSeries)):
+                tempdf = platformdf[platformdf["Platform"] == platformSeries[i]]
+                tempMean = tempdf["Global_Sales"].mean()
+                print("the global sales for their history for the {} were {} units".format(str(platformSeries[i]),int(tempMean * 1000000)))
+                
+        elif userInsight == 6:
+            EUSales = dataframe["EU_Sales"].mean()
+            NASales = dataframe["NA_Sales"].mean()
+            JPSales = dataframe["JP_Sales"].mean()
+            print("the sales for the EU were {} units".format(int(EUSales * 1000000)))
+            print("the sales for NA were {} units".format(int(NASales * 1000000)))
+            print("the sales for JP were {} units".format(int(JPSales * 1000000)))
+
+        elif userInsight == 7:
+            medianYoR = dataframe["Year_of_Release"].median()
+            print("the median year of release was {}".format(medianYoR))
+
+        elif userInsight == 8:
+            gameNames = dataframe[["Name","Critic_Score"]].copy()
+            minScore = minimumVal.loc["Critic_Score"]
+            print(gameNames.loc[gameNames["Critic_Score"] == minScore])
+            
 
     def option3():
         print("\nyou chose option 3")
@@ -76,6 +127,7 @@ with open("Video_Games_Sales_as_at_22_Dec_2016.csv","r") as file:
             print("2. display insights")
             print("3. option 3")
             print("0. quit")
+
 
     #create a loop to dispay the menu and read user input
     while True:
@@ -100,24 +152,4 @@ with open("Video_Games_Sales_as_at_22_Dec_2016.csv","r") as file:
             else:
                 print("invalid choice, try again") 
 
-def insightCode(userInsight):
-    if userInsight == "1":
-        maximiumVal = dataframe.max()
-        print(maximiumVal)
-    elif userInsight == "2":
-        pass
-    elif userInsight == "3":
-        pass
-    elif userInsight == "4":
-        pass
-    elif userInsight == "5":
-        pass
-    elif userInsight == "6":
-        pass
-    elif userInsight == "7":
-        pass
-    elif userInsight == "8":
-        pass
-    elif userInsight == "9":
-        pass
 
